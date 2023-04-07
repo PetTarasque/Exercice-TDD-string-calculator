3dd3ae52dd40c498600c8f14c44bd5eee24e271f
Hugo Pélissier-Jolin <hugo.pelissier-jolin.1@ulaval.ca>
2023-04-07 17:06:19 -0400

    Etape1

--- src/main/java/com/github/glo2003/Calculator.java
+++ src/main/java/com/github/glo2003/Calculator.java
@@ -1,11 +1,29 @@
 package com.github.glo2003;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        throw new InvalidNumberFormatException();
+        List<String> stringNumberList = new ArrayList<>();
+        String regex = ",+";
+        stringNumberList.addAll(Arrays.asList(numbers.split(regex)));
+        int sum;
+
+        try {
+            sum = stringNumberList.stream()
+                    .map(stringNumer -> Integer.parseInt(stringNumer))
+                    .reduce(0, Integer::sum);
+        } catch (NumberFormatException e) {
+            throw new InvalidNumberFormatException();
+        }
+
+        return sum;
     }
 }
--- src/test/java/com/github/glo2003/CalculatorTest.java
+++ src/test/java/com/github/glo2003/CalculatorTest.java
@@ -23,7 +23,35 @@ public class CalculatorTest {
     }
 
     @Test
-    void whenNonNumericValues_thenThrowInvalidInput() {
+    void whenSingleValue_thenReturnsValue() {
+        int result = calculator.add("1");
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void whenMultipleValue_thenReturnsTotal() {
+        int result = calculator.add("1,2");
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    void whenMissingValue_thenReturnsTotal() {
+        int result = calculator.add("1,");
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void whenMultipleSeparator_thenReturnsTotal() {
+        int result = calculator.add("1,,3");
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    void whenInvalidValue_thenThrowsInvalidNumberFromatException() {
         assertThrows(InvalidNumberFormatException.class,
                 () -> calculator.add("4,a"));
     }
